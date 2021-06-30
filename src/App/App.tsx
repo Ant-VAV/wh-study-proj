8228b36afc6275a68e8d76d40cddcc2fda45abe9
Anton Vdovichenko <ant-vav@skbkontur.ru>
2021-06-30 14:12:23 +0300

    Первые наброски в шапке

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React from 'react';
+import './App.css';
+import '../index.html';
+// @ts-ignore
+import logo from '../sources/logo.png';
 
 export default function App() {
-  return <h1>Hello, World</h1>
+  return <div className={'page'}>
+    <div className={'header'}>
+      <div className={'headerContent'}>
+        <div className={'logoAndSelector'}>
+          <img src={logo} width={'125px'} height={'100px'} alt='logo'/>
+          <button className={'selectCampaignButton'}>Выбор кампании</button>
+        </div>
+        <div className={'loginLink'}><a href={'#'}>Вход/Регистрация</a></div>
+      </div>
+    </div>
+    <div className={'greyLine'}>
+      <div className={'lineMenu'}>
+        Будут кнопки
+      </div>
+    </div>
+
+    <div className={'content'}>Тут контент</div>
+    <div className={'footer'}>Тут будет футер</div>
+  </div>;
 }
