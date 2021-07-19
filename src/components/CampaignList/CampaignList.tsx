591573acfad812138e26389bbc859144f61e6ab6
Anton Vdovichenko <ant-vav@skbkontur.ru>
2021-07-19 17:18:44 +0300

    Нормальная подгрузка данных

--- src/components/CampaignList/CampaignList.tsx
+++ src/components/CampaignList/CampaignList.tsx
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CampaignItem from "./CampaignItem";
-import { getCampaigns, getCampaignsSync } from "../../helpers/dataProvider";
+import { getCampaigns } from "../../helpers/dataProvider";
 import { ICampaignItem } from "../../helpers/interfaces";
 
 export default function CampaignList() {
-  const [campaigns, setCampaigns] = useState<ICampaignItem[]>(getCampaignsSync());
+  const [campaigns, setCampaigns] = useState<ICampaignItem[]>();
 
-  function getData() {
+  useEffect(() => {
     getCampaigns().then((data) => setCampaigns(data));
-  }
+  }, []);
 
   return <div>
     {campaigns && campaigns.map((elem) => {
-      return <CampaignItem element={elem} />
+      return <CampaignItem element={elem}/>
     })}
   </div>
 }
