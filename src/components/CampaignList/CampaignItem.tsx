3c7ff34c40bb43643a04c52cde2eda342fb571d6
Anton Vdovichenko <ant-vav@skbkontur.ru>
2021-07-21 17:23:12 +0300

    Один элемент списка кампаний

--- src/components/CampaignList/CampaignItem.tsx
+++ src/components/CampaignList/CampaignItem.tsx
@@ -7,9 +7,12 @@ interface ICampaignItemProp {
 }
 
 export default function CampaignItem(props: ICampaignItemProp) {
-  const {campaignName, campaignDescription} = props.element;
-  return <>
-    <h2>{campaignName}</h2>
-    <span>{campaignDescription}</span>
-  </>
+  const {campaignName, campaignDescription, campaignLogo} = props.element;
+  return <div className={'campaignItem'}>
+    <img className={'campaignImg'} src={`data:image/jpeg;base64,${campaignLogo}`} alt={'Campaign Logo Classified'}/>
+    <div className={'campaignDescriptionBlock'}>
+      <span className={'campaignName'}>{campaignName}</span>
+      <span className={'campaignDescription'}>{campaignDescription}</span>
+    </div>
+  </div>
 }
